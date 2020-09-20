aabc7c04d256e36df17575eee27086193c3aa963
QqWwEeRrTtYy-1 <71389292+QqWwEeRrTtYy-1@users.noreply.github.com>
2020-09-20 23:43:58 +0300

    Add files via upload

---
+++ random.py
@@ -0,0 +1,10 @@
+  
+import random
+
+while number < guess and number > guess:
+guess = int(input("Угадай число от 1-ого до 3-ех, которое загадал компьютер "))
+number = random.randinteger(1, 3)
+if number == guess:
+	print("Ты угадал!")
+	break
+input("Нажмите ENTER чтобы продолжить ")
