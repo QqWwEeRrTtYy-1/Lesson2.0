e57d69be0838f58e567a03f076234e7f2c5e86f2
QqWwEeRrTtYy-1 <71389292+QqWwEeRrTtYy-1@users.noreply.github.com>
2020-10-03 13:39:40 +0300

    Add files via upload

---
+++ SPISOK.py
@@ -0,0 +1,16 @@
+mylist = []
+mylist.append("Apple")
+mylist.append("Umbrella")
+mylist.insert(1,"Pen")
+print(mylist)
+mylist.remove("Umbrella")
+print(mylist)
+print(len(mylist))
+print(mylist)
+
+print("""
+	ABC
+	""")
+
+for counter in mylist:
+	print(counter)
