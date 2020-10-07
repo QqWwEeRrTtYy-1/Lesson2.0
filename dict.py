d5855e518977c4fa5dee446fbfc3fa4ea777e446
QqWwEeRrTtYy-1 <71389292+QqWwEeRrTtYy-1@users.noreply.github.com>
2020-10-07 17:45:09 +0300

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,2 @@
+mydict = {"name" : "Василий", "age" : 60, "occupation" : "президент"}
+print(mydict.get("name"))
---
+++ stuedents.py
@@ -0,0 +1,9 @@
+person1 = {"name" : "Иван", "age" : 18, "place" : 1}
+person2 = {"name" : "Виктор", "age" : 13, "place" : 2}
+person3 = {"name" : "Василий", "age" : 27, "place" : 3}
+person4 = {"name" : "Пётр", "age" : 16, "place" : 4}
+person5 = {"name" : "Алексей", "age" : 22, "place" : 5}
+
+students = [person1, person2, person3, person4, person5]
+
+print(students[0].get("name"))
