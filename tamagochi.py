f371a994442d7757a9b2aae506d1d2d5e4222dcb
QqWwEeRrTtYy-1 <71389292+QqWwEeRrTtYy-1@users.noreply.github.com>
2020-10-28 17:45:27 +0300

    Add files via upload

---
+++ tamagochi.py
@@ -0,0 +1,17 @@
+tamagoch = {
+    "Кличка:": "Федя",
+    "Счастье:": 80,
+    "Голод:": 75,
+    "Здоровье:": 100,
+    "Инструкция:": "\n 0 - покормить \n 1 - поиграть \n 2 - ничего не делать"
+
+}
+user_choise = 0
+def show_pet(tamagoch):
+	for key, value in tamagoch.items():
+		print(key, value)
+
+def feed_pet(tamagoch):
+	tamagoch[]
+
+show_pet(tamagoch)
