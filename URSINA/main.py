de66716df0c9da1cb9a1f115c5930d5539ca0953
QqWwEeRrTtYy-1 <71389292+QqWwEeRrTtYy-1@users.noreply.github.com>
2021-01-16 11:16:40 +0300

    Add files via upload

---
+++ URSINA/main.py
@@ -0,0 +1,21 @@
+import objects
+from ursina import *
+from ursina.prefabs.platformer_controller_2d import PlatformerController2d
+
+app = Ursina()
+
+
+player = PlatformerController2d(
+	position = (0, 0), 
+	collision = True,
+)
+Animation('image')
+
+
+camera.add_script(SmoothFollow(target=player, offset=[0.5, -30], speed=40))
+
+def update():
+    player.x += held_keys['d'] * time.dt * 2
+    player.x -= held_keys['a'] * time.dt * 2
+
+app.run()
---
+++ URSINA/objects.py
@@ -0,0 +1,59 @@
+from ursina import *
+from ursina.prefabs.platformer_controller_2d import PlatformerController2d
+
+surface_1 = Entity(
+	position = (-8, 3),
+	model = "cube",
+	color=color.green,
+	collider = 'box',
+	scale = (1, 11)
+	)
+surface = Entity(
+	position = (0, -2),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (15, 1)
+	)
+surface = Entity(
+	position = (20, 0),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (15, 1)
+	)
+surface = Entity(
+	position = (32, 4),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (2, 1)
+	)
+surface = Entity(
+	position = (40, 3),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (7, 1)
+	)
+surface = Entity(
+	position = (48, 2),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (1, 1)
+	)
+surface = Entity(
+	position = (56, 3),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (2, 1)
+	)
+surface = Entity(
+	position = (60, 3),
+	model = "cube",
+	color=color.green,
+	collider = "box",
+	scale = (1, 1)
+	)
